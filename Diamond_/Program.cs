1c6b050f71ac21c53a32900a91e5828d71ff9628
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-31 02:15:03 +0530

    Add project files.

---
+++ Diamond_/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Diamond_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of layer");
+            int A = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=A; i++)
+            {
+                for (int j = 1; j <= A - i; j++)
+
+                    Console.Write(" ");
+                for (int m = 1; m <=2 * i -1; m++)
+                    Console.Write("*");
+                Console.WriteLine();
+
+            }
+
+            for (int k = A-1; k >= 1; k--)
+            {
+                for (int j = 1; j <= A - k; j++)
+
+                    Console.Write(" ");
+                for (int m = 1; m <= 2 * k - 1; m++)
+                    Console.Write("*");
+                Console.WriteLine();
+
+
+
+
+            }
+
+
+
+
+
+
+            Console.WriteLine();
+        }
+    }
+}
